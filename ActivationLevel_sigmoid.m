4544c63f72d3bfb085ff93b33d931a39ba74c1be
Charlie Yan <yijia.yan@new.ox.ac.uk>
2021-04-29 11:36:08 +0100

    Update ActivationLevel_sigmoid.m

--- ActivationLevel_sigmoid.m
+++ ActivationLevel_sigmoid.m
@@ -11,16 +11,4 @@ function Activation = ActivationLevel_sigmoid(FRate, alpha, beta, gamma)
     Activation = log(1.0 ./ FRate - 1) ./ (-2 * beta) + alpha;
     Activation(Activation <= gamma) = 0;
 
-%     X = FRate;
-%     len_X = length(X);
-%     Activation = zeros(1, len_X);
-%     strength = beta / (beta + 2 / (alpha - gamma));
-%     for k = 1 : len_X
-%         if X(k) > strength
-%             Activation(k) = atanh((X(k) - strength) / (1 - strength)) / beta + alpha;
-%         elseif X(k) > 0
-%             Activation(k) = sqrt(X(k) / strength) * (alpha - gamma) + gamma;
-%         end
-%     end
-
 end
